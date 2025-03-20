1d1ef7576ab8a13f1f7c9fa6ba0e2ec8afc3933d
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-03-20 13:30:11 -0300

    feat: add i18n

--- src/renderer/src/pages/HomePage/HomePage.tsx
+++ src/renderer/src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,15 @@
+import { useTranslation } from 'react-i18next'
 import { useNavigate } from 'react-router-dom'
 
 export function HomePage(): JSX.Element {
   const navigate = useNavigate()
+  const {t} = useTranslation()
 
   return (
     <div>
       <p className="text-center text-red-500">Home Page</p>
       <button onClick={() => navigate('/settings')}>settings</button>
+      <p className='mt-10 text-center'>{t('helloWorld')}</p>
     </div>
   )
 }
