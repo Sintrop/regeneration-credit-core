cb2f4fe88ec3eef381a51ba867c4e2f8f0f48b07
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-03-31 14:10:21 -0300

    feat: add minimal logic vacancies

--- src/renderer/src/pages/RegisterPage/RegisterPage.tsx
+++ src/renderer/src/pages/RegisterPage/RegisterPage.tsx
@@ -8,8 +8,11 @@ import {
   RegistrationUserType,
   UserRegistration
 } from './components/UserRegistration/UserRegistration'
+import { Vacancies } from './components/Vacancies'
+import { useNavigate } from 'react-router-dom'
 
 export function RegisterPage(): JSX.Element {
+  const navigate = useNavigate()
   const { t } = useTranslation()
   const chainId = useChainId()
   const { address } = useAccount()
@@ -23,42 +26,67 @@ export function RegisterPage(): JSX.Element {
     args: [address]
   })
 
-  const userTypeWalletConnected = data as number
+  const userTypeWalletConnected = parseInt(data as string)
+
+  function handleGoToAccount(): void {
+    navigate('/account')
+  }
+
+  if (userTypeWalletConnected && userTypeWalletConnected !== 0) {
+    return (
+      <ScreenPage pageTitle={t('registration')}>
+        <div className="flex flex-col gap-5">
+          <p className="text-white">{t('youAlreadyRegistered')}</p>
+          <button
+            className={`bg-green-btn rounded-2xl px-10 h-10 text-white font-semibold mt-10 w-fit hover:cursor-pointer`}
+            onClick={handleGoToAccount}
+          >
+            {t('goToAccount')}
+          </button>
+        </div>
+      </ScreenPage>
+    )
+  }
 
   return (
     <ScreenPage pageTitle={t('registration')}>
-      <div className="flex flex-col">
-        {userTypeWalletConnected}
-        <p className="text-gray-300 text-sm">{t('walletConnected')}</p>
-        <div className="flex items-center gap-5 px-5 py-2 rounded-2xl bg-container-secondary w-fit">
-          <Jazzicon className="w-10 h-10" address={address as string} />
-          <p className="text-white">{address}</p>
+      <div className="flex flex-wrap gap-10 w-full">
+        <div className="flex flex-col max-w-[50%]">
+          <p className="text-gray-300 text-sm">{t('walletConnected')}</p>
+          <div className="flex items-center gap-5 px-5 py-2 rounded-2xl bg-container-secondary w-fit">
+            <Jazzicon className="w-10 h-10" address={address as string} />
+            <p className="text-white">{address}</p>
+          </div>
+
+          <p className="text-gray-300 text-sm mt-10">{t('yourName')}</p>
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="w-[450px] h-10 rounded-2xl bg-container-secondary px-5 text-white"
+            placeholder={t('typeHere')}
+          />
+
+          <p className="text-gray-300 text-sm mt-10">{t('selectUserType')}</p>
+          <select
+            className="w-[450px] h-10 rounded-2xl bg-container-secondary px-5 text-white"
+            value={userType}
+            onChange={(e) => setUserType(parseInt(e.target.value) as RegistrationUserType)}
+          >
+            <option value={1}>{t('regenerator')}</option>
+            <option value={2}>{t('inspector')}</option>
+            <option value={3}>{t('researcher')}</option>
+            <option value={4}>{t('developer')}</option>
+            <option value={5}>{t('contributor')}</option>
+            <option value={6}>{t('activist')}</option>
+            <option value={7}>{t('supporter')}</option>
+          </select>
+
+          <UserRegistration userType={userType} name={name} />
         </div>
 
-        <p className="text-gray-300 text-sm mt-10">{t('yourName')}</p>
-        <input
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          className="w-[450px] h-10 rounded-2xl bg-container-secondary px-5 text-white"
-          placeholder={t('typeHere')}
-        />
-
-        <p className="text-gray-300 text-sm mt-10">{t('selectUserType')}</p>
-        <select
-          className="w-[450px] h-10 rounded-2xl bg-container-secondary px-5 text-white"
-          value={userType}
-          onChange={(e) => setUserType(parseInt(e.target.value) as RegistrationUserType)}
-        >
-          <option value={1}>{t('regenerator')}</option>
-          <option value={2}>{t('inspector')}</option>
-          <option value={3}>{t('researcher')}</option>
-          <option value={4}>{t('developer')}</option>
-          <option value={5}>{t('contributor')}</option>
-          <option value={6}>{t('activist')}</option>
-          <option value={7}>{t('supporter')}</option>
-        </select>
-
-        <UserRegistration userType={userType} name={name} />
+        <div className="flex flex-col">
+          <Vacancies userType={userType} />
+        </div>
       </div>
     </ScreenPage>
   )
---
+++ src/renderer/src/pages/RegisterPage/components/Vacancies.tsx
@@ -0,0 +1,53 @@
+import { checkVacancies } from '@renderer/services/checkVacancies'
+import { sequoiaUserAbi, sequoiaUserAddress } from '@renderer/services/contracts'
+import { useTranslation } from 'react-i18next'
+import { useChainId, useReadContracts } from 'wagmi'
+
+interface Props {
+  userType: number
+}
+
+export function Vacancies({ userType }: Props): JSX.Element {
+  const { t } = useTranslation()
+  const chainId = useChainId()
+  const { data } = useReadContracts({
+    contracts: [
+      {
+        address: chainId === 1600 ? sequoiaUserAddress : sequoiaUserAddress,
+        abi: chainId === 1600 ? sequoiaUserAbi : sequoiaUserAbi,
+        functionName: 'userTypesCount',
+        args: [1]
+      },
+      {
+        address: chainId === 1600 ? sequoiaUserAddress : sequoiaUserAddress,
+        abi: chainId === 1600 ? sequoiaUserAbi : sequoiaUserAbi,
+        functionName: 'userTypesCount',
+        args: [userType]
+      }
+    ]
+  })
+
+  let availableVacancies: boolean = false
+
+  if (data) {
+    if (data[0].status === 'success' && data[0].status === 'success') {
+      const producersCount = parseInt(data[0].result as string)
+      const userTypeCount = parseInt(data[1].result as string)
+
+      const response = checkVacancies({ producersCount, userType, userTypeCount })
+      availableVacancies = response.availableVacancie
+    }
+  }
+
+  return (
+    <>
+      <p className="text-gray-300 text-sm">{t('vacancies')}</p>
+      <div className="flex flex-col p-3 rounded-2xl bg-container-primary ">
+        <p className="text-gray-300 text-sm">{t('descVacancies')}</p>
+        <p className="text-white">
+          {availableVacancies ? t('thereIsVacancie') : t('thereIsntVacancie')}
+        </p>
+      </div>
+    </>
+  )
+}
