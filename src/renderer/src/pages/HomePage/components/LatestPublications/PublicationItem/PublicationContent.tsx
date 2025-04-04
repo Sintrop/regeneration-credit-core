597f6c2ea7d3d0249e06bc2aeb96094607e664bc
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-04-04 18:57:21 -0300

    feat: create function to get image from ipfs

--- src/renderer/src/pages/HomePage/components/LatestPublications/PublicationItem/PublicationContent.tsx
+++ src/renderer/src/pages/HomePage/components/LatestPublications/PublicationItem/PublicationContent.tsx
@@ -1,17 +1,32 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { getImageFromIpfs } from '@renderer/services/ipfs'
+import { useEffect, useState } from 'react'
+
 interface Props {
   description?: string
   hashImage?: string
 }
 
 export function PublicationContent({ description, hashImage }: Props): JSX.Element {
+  const [imageSrc, setImageSrc] = useState<string | null>(null)
+
+  useEffect(() => {
+    handleGetImage()
+  }, [hashImage])
+
+  async function handleGetImage(): Promise<void> {
+    const response = await getImageFromIpfs(hashImage)
+    if (response.success) setImageSrc(response.image)
+  }
+
   return (
     <div className="flex flex-col mt-3">
       {description && <p className="text-white">{description}</p>}
 
-      {hashImage && (
+      {imageSrc && (
         <div className="w-full h-[400px] rounded-2xl mt-1 bg-container-secondary">
           <img
-            src={`https://ipfs.io/ipfs/${hashImage}`}
+            src={imageSrc}
             alt="Publication"
             className="w-full h-full object-cover rounded-2xl"
           />
--- src/renderer/src/services/ipfs.ts
+++ src/renderer/src/services/ipfs.ts
@@ -41,3 +41,26 @@ export function base64ToBlob(base64String: string): Blob {
   const byteArray = new Uint8Array(byteNumbers)
   return new Blob([byteArray], { type: 'image/png' })
 }
+
+interface ReturnGetImageFromIpfsProps {
+  success: boolean
+  image: string
+}
+export async function getImageFromIpfs(hash?: string): Promise<ReturnGetImageFromIpfsProps> {
+  const response = await fetch(`${IPFS_API_URL}/api/v0/cat?arg=${hash}`, { method: 'POST' })
+
+  if (response.ok) {
+    const blob = await response.blob()
+    const url = URL.createObjectURL(blob)
+
+    return {
+      success: true,
+      image: url
+    }
+  }
+
+  return {
+    success: false,
+    image: ''
+  }
+}
