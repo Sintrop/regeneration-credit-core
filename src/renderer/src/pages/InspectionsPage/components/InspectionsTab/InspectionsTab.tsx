6319ad1a1141dd36c89c2a5f2052166d23def86d
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-04-14 11:38:23 -0300

    fix: inspections table

--- src/renderer/src/pages/InspectionsPage/components/InspectionsTab/InspectionsTab.tsx
+++ src/renderer/src/pages/InspectionsPage/components/InspectionsTab/InspectionsTab.tsx
@@ -35,42 +35,29 @@ export function InspectionsTab(): JSX.Element {
       <p className="text-white">
         {t('inspectionsCount')}: {inspectionsCount}
       </p>
-      <div className="flex items-center bg-container-primary rounded-t-2xl px-5 h-10 border-b-2 border-container-secondary">
-        <div className="border-r border-container-secondary w-[50px]">
-          <p className="text-white font-semibold">ID</p>
-        </div>
-
-        <div className="border-r border-container-secondary flex-1 pl-5">
-          <p className="text-white font-semibold">{t('regenerator')}</p>
-        </div>
-
-        <div className="border-r border-container-secondary flex-1 pl-5">
-          <p className="text-white font-semibold">{t('inspector')}</p>
-        </div>
-
-        <div className="border-r border-container-secondary w-[120px] pl-5">
-          <p className="text-white font-semibold">{t('status')}</p>
-        </div>
-
-        <div className="border-r border-container-secondary w-[120px] pl-5">
-          <p className="text-white font-semibold">{t('score')}</p>
-        </div>
-
-        <div className="border-r border-container-secondary w-[120px] pl-5">
-          <p className="text-white font-semibold">{t('actions')}</p>
-        </div>
-      </div>
 
       {inspectionsIds.length === 0 ? (
         <div className="items-center mt-10">
           <p className="text-white text-center">{t('anyInspectionsAvailable')}</p>
         </div>
       ) : (
-        <>
-          {inspectionsIds.map((id, index) => (
-            <InspectionItem key={index} id={id} />
-          ))}
-        </>
+        <table className="min-w-full border-collapse bg-container-primary rounded-xl overflow-hidden">
+          <thead>
+            <tr className="border-b border-container-secondary text-white">
+              <th className="p-2 border-r border-container-secondary">ID</th>
+              <th className="p-2 border-r border-container-secondary">{t('regenerator')}</th>
+              <th className="p-2 border-r border-container-secondary">{t('inspector')}</th>
+              <th className="p-2 border-r border-container-secondary">{t('status')}</th>
+              <th className="p-2 border-r border-container-secondary">{t('score')}</th>
+              <th className="p-2">{t('actions')}</th>
+            </tr>
+          </thead>
+          <tbody>
+            {inspectionsIds.map((id, index) => (
+              <InspectionItem key={index} id={id} />
+            ))}
+          </tbody>
+        </table>
       )}
     </div>
   )
