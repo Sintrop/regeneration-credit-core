d8cba35fdcebab84d003707bf60af8f8f32982c0
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-04-15 12:18:11 -0300

    fix: supporters table

--- src/renderer/src/pages/UsersPage/components/Supporters/Supporters.tsx
+++ src/renderer/src/pages/UsersPage/components/Supporters/Supporters.tsx
@@ -13,41 +13,29 @@ export function Supporters({ idsList }: Props): JSX.Element {
       <p className="text-white">
         {t('supportersCount')}: {idsList.length}
       </p>
-      <div className="flex flex-col overflow-x-auto bg-container-primary rounded-2xl">
-        <div className="flex items-center px-5 h-10 border-b-2 border-container-secondary">
-          <div className="border-r border-container-secondary min-w-[50px]">
-            <p className="text-white font-semibold">ID</p>
-          </div>
 
-          <div className="border-r border-container-secondary min-w-[400px] pl-5">
-            <p className="text-white font-semibold">{t('wallet')}</p>
-          </div>
-
-          <div className="border-r border-container-secondary min-w-[300px] pl-5">
-            <p className="text-white font-semibold">{t('name')}</p>
-          </div>
-
-          <div className="border-r border-container-secondary min-w-[120px] pl-5">
-            <p className="text-white font-semibold">{t('createdAt')}</p>
-          </div>
-
-          <div className="border-r border-container-secondary min-w-[120px] pl-5">
-            <p className="text-white font-semibold">{t('actions')}</p>
-          </div>
+      {idsList.length === 0 ? (
+        <div className="items-center mt-10">
+          <p className="text-white text-center">{t('anySupporterRegistered')}</p>
         </div>
-
-        {idsList.length === 0 ? (
-          <div className="items-center mt-10">
-            <p className="text-white text-center">{t('anySupporterRegistered')}</p>
-          </div>
-        ) : (
-          <>
+      ) : (
+        <table className="min-w-full border-collapse bg-container-primary rounded-xl overflow-hidden">
+          <thead>
+            <tr className="border-b border-container-secondary text-white">
+              <th className="p-2 border-r border-container-secondary">ID</th>
+              <th className="p-2 border-r border-container-secondary">{t('wallet')}</th>
+              <th className="p-2 border-r border-container-secondary">{t('name')}</th>
+              <th className="p-2 border-r border-container-secondary">{t('createdAt')}</th>
+              <th className="p-2">{t('actions')}</th>
+            </tr>
+          </thead>
+          <tbody>
             {idsList.map((id, index) => (
               <SupporterItem key={index} id={id} />
             ))}
-          </>
-        )}
-      </div>
+          </tbody>
+        </table>
+      )}
     </div>
   )
 }
