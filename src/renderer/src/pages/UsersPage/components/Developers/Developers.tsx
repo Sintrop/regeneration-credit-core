652a344a2e71a57c84d401bb139ce9cb14c708ee
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-04-15 11:49:58 -0300

    fix: developers table

--- src/renderer/src/pages/UsersPage/components/Developers/Developers.tsx
+++ src/renderer/src/pages/UsersPage/components/Developers/Developers.tsx
@@ -13,49 +13,31 @@ export function Developers({ idsList }: Props): JSX.Element {
       <p className="text-white">
         {t('developersCount')}: {idsList.length}
       </p>
-      <div className="flex flex-col overflow-x-auto bg-container-primary rounded-2xl">
-        <div className="flex items-center px-5 h-10 border-b-2 border-container-secondary">
-          <div className="border-r border-container-secondary min-w-[50px]">
-            <p className="text-white font-semibold">ID</p>
-          </div>
 
-          <div className="border-r border-container-secondary min-w-[400px] pl-5">
-            <p className="text-white font-semibold">{t('wallet')}</p>
-          </div>
-
-          <div className="border-r border-container-secondary min-w-[300px] pl-5">
-            <p className="text-white font-semibold">{t('name')}</p>
-          </div>
-
-          <div className="border-r border-container-secondary min-w-[120px] pl-5">
-            <p className="text-white font-semibold">{t('createdAt')}</p>
-          </div>
-
-          <div className="border-r border-container-secondary min-w-[120px] pl-5">
-            <p className="text-white font-semibold">{t('totalReports')}</p>
-          </div>
-
-          <div className="border-r border-container-secondary min-w-[100px] pl-5">
-            <p className="text-white font-semibold">{t('level')}</p>
-          </div>
-
-          <div className="border-r border-container-secondary min-w-[120px] pl-5">
-            <p className="text-white font-semibold">{t('actions')}</p>
-          </div>
+      {idsList.length === 0 ? (
+        <div className="flex flex-col mt-10 items-center">
+          <p className="text-white">{t('anyDevelopersRegistered')}</p>
         </div>
-
-        {idsList.length === 0 ? (
-          <div className="flex flex-col mt-10 items-center">
-            <p className="text-white">{t('anyDevelopersRegistered')}</p>
-          </div>
-        ) : (
-          <>
+      ) : (
+        <table className="min-w-full border-collapse bg-container-primary rounded-xl overflow-hidden">
+          <thead>
+            <tr className="border-b border-container-secondary text-white">
+              <th className="p-2 border-r border-container-secondary">ID</th>
+              <th className="p-2 border-r border-container-secondary">{t('wallet')}</th>
+              <th className="p-2 border-r border-container-secondary">{t('name')}</th>
+              <th className="p-2 border-r border-container-secondary">{t('createdAt')}</th>
+              <th className="p-2 border-r border-container-secondary">{t('totalReports')}</th>
+              <th className="p-2 border-r border-container-secondary">{t('level')}</th>
+              <th className="p-2">{t('actions')}</th>
+            </tr>
+          </thead>
+          <tbody>
             {idsList.map((id, index) => (
               <DeveloperItem key={index} id={id} />
             ))}
-          </>
-        )}
-      </div>
+          </tbody>
+        </table>
+      )}
     </div>
   )
 }
