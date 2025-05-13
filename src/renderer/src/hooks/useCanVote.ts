958ec443875c2cff1dbb141ef8c7ab3b293dbcdc
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-05-13 13:46:42 -0300

    feat: create hook to check can vote

---
+++ src/renderer/src/hooks/useCanVote.ts
@@ -0,0 +1,65 @@
+import {
+  sequoiaUserAbi,
+  sequoiaUserAddress,
+  sequoiaValidationAbi,
+  sequoiaValidationAddress,
+  userAbi,
+  userAddress,
+  validationAbi,
+  validationAddress
+} from '@renderer/services/contracts'
+import { useEffect, useState } from 'react'
+import { useChainId, useReadContract } from 'wagmi'
+
+interface ReturnUseCanVote {
+  isLoading: boolean
+  canVote: boolean
+  canVoteThisResource: boolean
+}
+interface Props {
+  address: string
+  resource: 'inspection' | 'report' | 'contribution' | 'research'
+}
+export function useCanVote({ address, resource }: Props): ReturnUseCanVote {
+  const chainId = useChainId()
+  const [canVoteThisResource, setCanVoteThisResource] = useState(false)
+
+  const { data: responseGetUser, isLoading: loadingUser } = useReadContract({
+    address: chainId === 250225 ? userAddress : sequoiaUserAddress,
+    abi: chainId === 250225 ? userAbi : sequoiaUserAbi,
+    functionName: 'getUser',
+    args: [address]
+  })
+
+  const userType = responseGetUser as number
+
+  const { data: responseCanVote, isLoading: loadingCanVote } = useReadContract({
+    address: chainId === 250225 ? validationAddress : sequoiaValidationAddress,
+    abi: chainId === 250225 ? validationAbi : sequoiaValidationAbi,
+    functionName: 'waitedTimeBetweenVotes',
+    args: [address]
+  })
+
+  const canVote = responseCanVote as boolean
+
+  useEffect(() => {
+    if (userType) {
+      checkCanVoteThisResource(userType)
+    }
+  }, [userType])
+
+  function checkCanVoteThisResource(userType: number): void {
+    if (resource === 'inspection') {
+      const findedUserType = usersVoteInspection.find((item) => item === userType)
+      if (findedUserType) setCanVoteThisResource(true)
+    }
+  }
+
+  return {
+    isLoading: loadingCanVote || loadingUser,
+    canVote: canVote ? canVote : false,
+    canVoteThisResource
+  }
+}
+
+const usersVoteInspection = [3, 4, 5, 6]
