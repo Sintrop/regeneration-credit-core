44b058f66e32922f60ad97f75ac074535938a0d9
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-05-13 15:25:58 -0300

    feat: add proof photo result inspection

--- src/renderer/src/pages/ResourceDetailsPage/components/ResourceData/InspectionData.tsx
+++ src/renderer/src/pages/ResourceDetailsPage/components/ResourceData/InspectionData.tsx
@@ -8,6 +8,7 @@ import {
 } from '@renderer/services/contracts'
 import { InspectionProps } from '@renderer/types/inspection'
 import { useTranslation } from 'react-i18next'
+import { Img } from 'react-image'
 import { formatUnits } from 'viem'
 import { useChainId, useReadContract } from 'wagmi'
 
@@ -32,6 +33,7 @@ export function InspectionData({ id, setReport, setValidationsCount }: Props): J
   if (inspection) {
     setReport(inspection.report)
     setValidationsCount(parseInt(formatUnits(BigInt(inspection.validationsCount), 0)))
+    console.log(inspection)
   }
 
   if (isLoading) {
@@ -46,49 +48,63 @@ export function InspectionData({ id, setReport, setValidationsCount }: Props): J
 
   return (
     <div className="flex flex-col">
-      <div className="flex flex-col gap-2">
-        <div className="flex items-center gap-2">
-          <p className="text-white">{t('regenerator')}:</p>
-          <UserAddressLink address={inspection.regenerator} />
+      <div className="flex gap-10">
+        <div className="flex flex-col gap-2">
+          <div className="flex items-center gap-2">
+            <p className="text-white">{t('regenerator')}:</p>
+            <UserAddressLink address={inspection.regenerator} />
+          </div>
+
+          <div className="flex items-center gap-2">
+            <p className="text-white">{t('inspector')}:</p>
+            <UserAddressLink address={inspection.inspector} />
+          </div>
+
+          <div className="flex items-center gap-2">
+            <p className="text-white">{t('proofPhoto')}:</p>
+            <p className="text-white">{inspection && inspection.proofPhoto}</p>
+          </div>
+
+          <div className="flex items-center gap-2">
+            <p className="text-white">{t('createdAt')}:</p>
+            <p className="text-white">
+              {inspection && formatUnits(BigInt(inspection.createdAt), 0)}
+            </p>
+          </div>
+
+          <div className="flex items-center gap-2">
+            <p className="text-white">{t('acceptedAt')}:</p>
+            <p className="text-white">
+              {inspection && formatUnits(BigInt(inspection.acceptedAt), 0)}
+            </p>
+          </div>
+
+          <div className="flex items-center gap-2">
+            <p className="text-white">{t('inspectedAt')}:</p>
+            <p className="text-white">
+              {inspection && formatUnits(BigInt(inspection.inspectedAt), 0)}
+            </p>
+          </div>
+
+          <div className="flex items-center gap-2">
+            <p className="text-white">{t('inspectedAtEra')}:</p>
+            <p className="text-white">
+              {inspection && formatUnits(BigInt(inspection.inspectedAtEra), 0)}
+            </p>
+          </div>
+
+          <div className="flex items-center gap-2">
+            <p className="text-white">{t('validationsCount')}:</p>
+            <p className="text-white">
+              {inspection && formatUnits(BigInt(inspection.validationsCount), 0)}
+            </p>
+          </div>
         </div>
 
-        <div className="flex items-center gap-2">
-          <p className="text-white">{t('inspector')}:</p>
-          <UserAddressLink address={inspection.inspector} />
-        </div>
-
-        <div className="flex items-center gap-2">
-          <p className="text-white">{t('createdAt')}:</p>
-          <p className="text-white">{inspection && formatUnits(BigInt(inspection.createdAt), 0)}</p>
-        </div>
-
-        <div className="flex items-center gap-2">
-          <p className="text-white">{t('acceptedAt')}:</p>
-          <p className="text-white">
-            {inspection && formatUnits(BigInt(inspection.acceptedAt), 0)}
-          </p>
-        </div>
-
-        <div className="flex items-center gap-2">
-          <p className="text-white">{t('inspectedAt')}:</p>
-          <p className="text-white">
-            {inspection && formatUnits(BigInt(inspection.inspectedAt), 0)}
-          </p>
-        </div>
-
-        <div className="flex items-center gap-2">
-          <p className="text-white">{t('inspectedAtEra')}:</p>
-          <p className="text-white">
-            {inspection && formatUnits(BigInt(inspection.inspectedAtEra), 0)}
-          </p>
-        </div>
-
-        <div className="flex items-center gap-2">
-          <p className="text-white">{t('validationsCount')}:</p>
-          <p className="text-white">
-            {inspection && formatUnits(BigInt(inspection.validationsCount), 0)}
-          </p>
-        </div>
+        <Img
+          src={`https://ipfs.io/ipfs/${inspection.proofPhoto}`}
+          className="w-[200px] h-[200px] rounded-2xl object-cover"
+        />
       </div>
 
       <div className="flex items-center gap-5 mt-2">
