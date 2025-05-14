322bc317f2535fa69677e64048d111abc8ef110e
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-05-14 11:33:22 -0300

    feat: create hook to check if user can publish work

---
+++ src/renderer/src/hooks/useCanPublishWork.ts
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react'
+import { formatUnits } from 'viem'
+import { useBlockNumber } from 'wagmi'
+
+interface ReturnUseCanPublishWork {
+  canPublish: boolean
+  canPublishIn: number
+  isLoading: boolean
+}
+interface Props {
+  lastPublishedAt?: number
+}
+export function useCanPublishWork({ lastPublishedAt }: Props): ReturnUseCanPublishWork {
+  const { data: responseBlock } = useBlockNumber()
+  const blockNumber = responseBlock ? parseInt(formatUnits(BigInt(responseBlock), 0)) : 0
+
+  const [isLoading, setIsLoading] = useState(true)
+  const [canPublish, setCanPublish] = useState(false)
+  const [canPublishIn, setCanPublishIn] = useState(0)
+
+  useEffect(() => {
+    checkCanPublish()
+
+    if (blockNumber === 0) {
+      setIsLoading(true)
+    } else {
+      setIsLoading(false)
+    }
+  }, [blockNumber])
+
+  function checkCanPublish(): void {
+    if (!lastPublishedAt) return
+    const timeBetweenWorks = parseInt(import.meta.env.VITE_TIME_BETWEEN_WORKS)
+    const canPublishBlock = lastPublishedAt + timeBetweenWorks
+
+    if (canPublishBlock < blockNumber) {
+      setCanPublish(true)
+    } else {
+      setCanPublish(false)
+      setCanPublishIn(canPublishBlock - blockNumber)
+    }
+  }
+
+  return {
+    canPublish,
+    canPublishIn,
+    isLoading
+  }
+}
