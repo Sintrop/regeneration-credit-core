d36928b61a6b99cfe206716d2a9c57dfe3daa1b7
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-05-19 14:47:28 -0300

    feat: improve publication research feed

--- src/renderer/src/pages/HomePage/components/FeedTabs/ResearchesFeedTab/ResearcheFeedItem/ResearcheFeedContent.tsx
+++ src/renderer/src/pages/HomePage/components/FeedTabs/ResearchesFeedTab/ResearcheFeedItem/ResearcheFeedContent.tsx
@@ -1,19 +1,34 @@
-import { PdfHashLink } from '@renderer/components/PdfHashLink/PdfHashLink';
 import { useTranslation } from 'react-i18next'
+import { useNavigate } from 'react-router-dom';
 
 interface Props {
-  era: number;
-  report: string;
+  researchId: number
+  title: string
+  thesis: string
 }
 
-export function ResearcheFeedContent({ era, report }: Props): JSX.Element {
+export function ResearcheFeedContent({ researchId, thesis, title }: Props): JSX.Element {
   const { t } = useTranslation();
+  const navigate = useNavigate()
+
+  function handleGoToResourceDetails(): void {
+    navigate(`/resource-details/researche/${researchId}`)
+  }
 
   return (
     <div className="flex flex-col mt-2">
-      <p className="text-white">Era: {era}</p>
-      <p className="text-sm text-gray-300 mt-2">{t('research')}</p>
-      <PdfHashLink hash={report} />
+      <p className="text-gray-300 text-sm text-center">{t('publishedAResearch')}</p>
+      <p className="text-gray-300 text-sm mt-2">{t('title')}</p>
+      <p className="text-white">{title}</p>
+      <p className="text-sm text-gray-300 mt-2">{t('thesis')}</p>
+      <p className="text-white">{thesis}</p>
+
+      <button
+        className="text-green-500 underline hover:cursor-pointer mt-3"
+        onClick={handleGoToResourceDetails}
+      >
+        {t('seeResearch')}
+      </button>
     </div>
   );
 }
--- src/renderer/src/pages/HomePage/components/FeedTabs/ResearchesFeedTab/ResearcheFeedItem/ResearcheFeedItem.tsx
+++ src/renderer/src/pages/HomePage/components/FeedTabs/ResearchesFeedTab/ResearcheFeedItem/ResearcheFeedItem.tsx
@@ -34,19 +34,20 @@ export function ResearcheFeedItem({ id }: Props): JSX.Element {
 
   const researcher = data2 as ResearcherProps
 
-  return (
-    <div className="flex flex-col rounded-2xl p-3 bg-container-primary w-full">
-      <HeaderFeedItem
-        name={researcher && researcher.name}
-        proofPhoto={researcher && researcher.proofPhoto}
-        publishedAt={research && formatUnits(BigInt(research.createdAtBlock), 0)}
-        address={research && research.createdBy}
-      />
+  if (researcher && research) {
+    return (
+      <div className="flex flex-col rounded-2xl p-3 bg-container-primary w-full">
+        <HeaderFeedItem
+          name={researcher.name}
+          proofPhoto={researcher.proofPhoto}
+          publishedAt={formatUnits(BigInt(research.createdAtBlock), 0)}
+          address={research.createdBy}
+        />
 
-      <ResearcheFeedContent
-        era={research ? parseInt(formatUnits(BigInt(research.era), 0)) : 0}
-        report={research ? research.file : ''}
-      />
-    </div>
-  )
+        <ResearcheFeedContent title={research.title} thesis={research.thesis} researchId={id} />
+      </div>
+    )
+  }
+
+  return <div />
 }
