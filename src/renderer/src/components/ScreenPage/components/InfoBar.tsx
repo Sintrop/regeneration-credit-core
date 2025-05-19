0126ff776e02cbf8496499a9d24fd5e8bc45e85e
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-05-19 15:21:38 -0300

    feat: create hook use era

--- src/renderer/src/components/ScreenPage/components/InfoBar.tsx
+++ src/renderer/src/components/ScreenPage/components/InfoBar.tsx
@@ -1,47 +1,9 @@
-import {
-  regeneratorPoolAbi,
-  regeneratorPoolAddress,
-  sequoiaRegeneratorPoolAbi,
-  sequoiaRegeneratorPoolAddress
-} from '@renderer/services/contracts'
+import { useEra } from '@renderer/hooks/useEra'
 import { useTranslation } from 'react-i18next'
-import { formatUnits } from 'viem'
-import { useChainId, useReadContract } from 'wagmi'
 
 export function InfoBar(): JSX.Element {
   const { t } = useTranslation()
-  const chainId = useChainId()
-
-  const { data: responseCurrentEra } = useReadContract({
-    address: chainId === 250225 ? regeneratorPoolAddress : sequoiaRegeneratorPoolAddress,
-    abi: chainId === 250225 ? regeneratorPoolAbi : sequoiaRegeneratorPoolAbi,
-    functionName: 'currentContractEra'
-  })
-
-  const currentEra = responseCurrentEra ? formatUnits(BigInt(responseCurrentEra as string), 0) : 0
-
-  const { data: responseNextEra } = useReadContract({
-    address: chainId === 250225 ? regeneratorPoolAddress : sequoiaRegeneratorPoolAddress,
-    abi: chainId === 250225 ? regeneratorPoolAbi : sequoiaRegeneratorPoolAbi,
-    functionName: 'nextEraIn',
-    args: [currentEra]
-  })
-
-  const nextEra = responseNextEra
-    ? Intl.NumberFormat('pt-BR').format(
-        parseFloat(formatUnits(BigInt(responseNextEra as string), 0))
-      )
-    : 0
-
-  const { data: responseCurrentEpoch } = useReadContract({
-    address: chainId === 250225 ? regeneratorPoolAddress : sequoiaRegeneratorPoolAddress,
-    abi: chainId === 250225 ? regeneratorPoolAbi : sequoiaRegeneratorPoolAbi,
-    functionName: 'currentEpoch'
-  })
-
-  const currentEpoch = responseCurrentEpoch
-    ? formatUnits(BigInt(responseCurrentEpoch as string), 0)
-    : 0
+  const { currentEpoch, currentEra, nextEraIn } = useEra()
 
   return (
     <div className="flex items-center gap-8 w-full min-h-10 bg-green-700 px-5">
@@ -52,7 +14,7 @@ export function InfoBar(): JSX.Element {
       <p className="text-gray-300 text-sm">
         {t('nextEraIn')}:{' '}
         <span className="text-white font-semibold text-base">
-          {nextEra} {t('blocks')}
+          {Intl.NumberFormat('pt-BR').format(nextEraIn)} {t('blocks')}
         </span>
       </p>
 
