83022e29fbb785d4c1fdbc368ad2a537e65e0430
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-07-30 13:54:47 -0300

    update source address from code to env

--- src/renderer/src/services/contracts.ts
+++ src/renderer/src/services/contracts.ts
@@ -19,57 +19,60 @@ import SequoiaInspectionContract from '../../../data/abis/sequoia/InspectionRule
 import SequoiaInvitationContract from '../../../data/abis/sequoia/InvitationRules.json'
 import SequoiaVoteContract from '../../../data/abis/sequoia/VoteRules.json'
 import SequoiaValidationContract from '../../../data/abis/sequoia/ValidationRules.json'
+import SequoiaRcIndexContract from '../../../data/abis/sequoia/RegenerationIndexRules.json'
 
 import { ContractListProps } from '@renderer/types/contract'
 
 //Address contracts
 ///////////////////////testnet
-export const sequoiaRcAddress = '0xfA6f7591b9445C87C2E2C648eC157a6a47A9Bafe'
-export const sequoiaRcImpactAddress = '0x2AbC2E083Be9bFfCDb264cB7C29411B69eb2D801'
-export const sequoiaUserAddress = '0x37D12613EA00CcD9B0917909Da205298FfBAACe6'
-export const sequoiaRegeneratorAddress = '0x09FC30D44BCb401318bdD296d4ca247DF7ddb10D'
-export const sequoiaRegeneratorPoolAddress = '0x806d51A01Fbc2Cc5B1BF7555dFF7cB769c281cA7'
-export const sequoiaInspectorAddress = '0x1ca2261AF70b8f658c60eB71cE7cF2370578d147'
-export const sequoiaInspectorPoolAddress = '0x7f592878A8599aAEd15C58d7689B8F73C8764D17'
-export const sequoiaResearcherAddress = '0x4D642934fa992718d955ad902576D596b80BC322'
-export const sequoiaResearcherPoolAddress = '0x56470F9F5F143f056a9Aa51045760ACF31727f5C'
-export const sequoiaDeveloperAddress = '0xCa8552bA00246533Fe6f573a8a26c5cd83a83941'
-export const sequoiaDeveloperPoolAddress = '0x1acFef1A7a3CC9eB8eCae6edc09447b12097e036'
-export const sequoiaContributorAddress = '0x76a3702283e343C1d1cf675a1b8d477C3A0DD360'
-export const sequoiaContributorPoolAddress = '0x9cE4dE8b585e1cd79Df9CaD155822A64E2E5Ed83'
-export const sequoiaActivistAddress = '0xee1fE9b7A68d368EB75bCf0aD954DDC14aFb9528'
-export const sequoiaActivistPoolAddress = '0xb578d0824819BB50829b028Dd33901863A090224'
-export const sequoiaSupporterAddress = '0xE26fE4B77e8D57753505fD2277E921E5A80912EC'
-export const sequoiaInspectionAddress = '0x792fA564Ff4c22f46A4896dBD4dDB964F3e38eb4'
-export const sequoiaInvitationAddress = '0x5695A9742699D584764DAbF5619eE06D1D4D489C'
-export const sequoiaVoteAddress = '0x0D74B26f738518c9160Fd9A118F28F58997875eb'
-export const sequoiaValidationAddress = '0xa314ea72e73f9bC4E937966E1c8BccfC49Dde92B'
+export const sequoiaRcAddress = import.meta.env.VITE_SEQUOIA_RC_ADDRESS
+export const sequoiaRcImpactAddress = import.meta.env.VITE_SEQUOIA_RC_IMPACT_ADDRESS
+export const sequoiaUserAddress = import.meta.env.VITE_SEQUOIA_USER_ADDRESS
+export const sequoiaRegeneratorAddress = import.meta.env.VITE_SEQUOIA_REGENERATOR_ADDRESS
+export const sequoiaRegeneratorPoolAddress = import.meta.env.VITE_SEQUOIA_REGENERATOR_POOL_ADDRESS
+export const sequoiaInspectorAddress = import.meta.env.VITE_SEQUOIA_INSPECTOR_ADDRESS
+export const sequoiaInspectorPoolAddress = import.meta.env.VITE_SEQUOIA_INSPECTOR_POOL_ADDRESS
+export const sequoiaResearcherAddress = import.meta.env.VITE_SEQUOIA_RESEARCHER_ADDRESS
+export const sequoiaResearcherPoolAddress = import.meta.env.VITE_SEQUOIA_RSEARCHER_POOL_ADDRESS
+export const sequoiaDeveloperAddress = import.meta.env.VITE_SEQUOIA_DEVELOPER_ADDRESS
+export const sequoiaDeveloperPoolAddress = import.meta.env.VITE_SEQUOIA_DEVELOPER_POOL_ADDRESS
+export const sequoiaContributorAddress = import.meta.env.VITE_SEQUOIA_CONTRIBUTOR_ADDRESS
+export const sequoiaContributorPoolAddress = import.meta.env.VITE_SEQUOIA_CONSTRIBUTOR_POOL_ADDRESS
+export const sequoiaActivistAddress = import.meta.env.VITE_SEQUOIA_ACTIVIST_ADDRESS
+export const sequoiaActivistPoolAddress = import.meta.env.VITE_SEQUOIA_ACTIVIST_POOL_ADDRESS
+export const sequoiaSupporterAddress = import.meta.env.VITE_SEQUOIA_SUPPORTER_ADDRESS
+export const sequoiaInspectionAddress = import.meta.env.VITE_SEQUOIA_INSPECTION_ADDRESS
+export const sequoiaInvitationAddress = import.meta.env.VITE_SEQUOIA_INVITATION_ADDRESS
+export const sequoiaVoteAddress = import.meta.env.VITE_SEQUOIA_VOTE_ADDRESS
+export const sequoiaValidationAddress = import.meta.env.VITE_SEQUOIA_VALIDATION_ADDRESS
+export const sequoiaRcIndexAddress = import.meta.env.VITE_SEQUOIA_RC_INDEX_ADDRESS
 
 export const sequoiaSupporterPoolAddress = '0x751869D00C8C281D5337Cf15623e3537C98F2Df3'
 
 // future mainnet
-export const rcAddress = '0xB795F59fd970c5272dca65455306030EA08184d7'
-export const rcImpactAddress = '0xB800124b6C91c17FD63706B1f7ebF0d6AEc9031A'
-export const userAddress = '0x4EF987b3c58A1E1077c723CF87811f3b015Ea5E6'
-export const regeneratorAddress = '0xAE4D3f9D7698B1964Ae186bb444fD96ad8d7Cd6e'
-export const regeneratorPoolAddress = '0x6D95bbeef2c53EdDC4dbD47fbc6249F40E621822'
-export const inspectorAddress = '0xCDA37909e2CF5A5D8E7a5B0c2d6BBB99feE26404'
-export const inspectorPoolAddress = '0x8A13E6c091ec35157c0492740108EBED8152597e'
-export const researcherAddress = '0x6741e7A944A4aedC0e52Bc11376C13d902C50Dc9'
-export const researcherPoolAddress = '0x678d6F552E826862B2F7Fb1b9e26FF81ED08bd8D'
-export const developerAddress = '0x99868e83E7fECb934FfE5B1CA7FaD76a86163c4A'
-export const developerPoolAddress = '0x2632f7b1dc0f27cb3Acd3C24f231dFc5C77292e3'
-export const contributorAddress = '0x85ce790326528f520E90DeFf1e14bA9A17A539a1'
-export const contributorPoolAddress = '0xB898f92c0010B1E8801831955CeDDa75af56Ba19'
-export const activistAddress = '0xcD641461795895D9De496a4b2f20C20E18963DC6'
-export const activistPoolAddress = '0xcCBaF746E2C754C512BDE452AeB4691c0C5055f6'
-export const supporterAddress = '0xCC0e9CfE2302A0f861aDdFb2649edEBbAFa47855'
-export const supporterPoolAddress = '0x8e2cf67005Cc5F0ad7B470BD038f7B0fc3de0b58'
-export const inspectionAddress = '0xC2d9b310a7BC64bc0Fba5A10c1d93635a4aA90C4'
-export const invitationAddress = '0x74F92a6e87e68B82e84D902b2a9659fb1AfA3893'
-export const voteAddress = '0x415259228A9122520CeE88cDAEdf465f17E0aA9f'
-export const validationAddress = '0x66F7e2b9fa479D1771A63D2a3955f8aB7A05545C'
+export const rcAddress = import.meta.env.VITE_RC_ADDRESS
+export const rcImpactAddress = import.meta.env.VITE_RC_IMPACT_ADDRESS
+export const userAddress = import.meta.env.VITE_USER_ADDRESS
+export const regeneratorAddress = import.meta.env.VITE_REGENERATOR_ADDRESS
+export const regeneratorPoolAddress = import.meta.env.VITE_REGENERATOR_POOL_ADDRESS
+export const inspectorAddress = import.meta.env.VITE_INSPECTOR_ADDRESS
+export const inspectorPoolAddress = import.meta.env.VITE_INSPECTOR_POOL_ADDRESS
+export const researcherAddress = import.meta.env.VITE_RESEARCHER_ADDRESS
+export const researcherPoolAddress = import.meta.env.VITE_RSEARCHER_POOL_ADDRESS
+export const developerAddress = import.meta.env.VITE_DEVELOPER_ADDRESS
+export const developerPoolAddress = import.meta.env.VITE_DEVELOPER_POOL_ADDRESS
+export const contributorAddress = import.meta.env.VITE_CONTRIBUTOR_ADDRESS
+export const contributorPoolAddress = import.meta.env.VITE_CONSTRIBUTOR_POOL_ADDRESS
+export const activistAddress = import.meta.env.VITE_ACTIVIST_ADDRESS
+export const activistPoolAddress = import.meta.env.VITE_ACTIVIST_POOL_ADDRESS
+export const supporterAddress = import.meta.env.VITE_SUPPORTER_ADDRESS
+export const inspectionAddress = import.meta.env.VITE_INSPECTION_ADDRESS
+export const invitationAddress = import.meta.env.VITE_INVITATION_ADDRESS
+export const voteAddress = import.meta.env.VITE_VOTE_ADDRESS
+export const validationAddress = import.meta.env.VITE_VALIDATION_ADDRESS
+export const rcIndexAddress = import.meta.env.VITE_RC_INDEX_ADDRESS
 
+export const supporterPoolAddress = import.meta.env.VITE_RC_ADDRESS
 
 //Abis contracts
 /////////////////// testnet
@@ -94,6 +97,7 @@ export const sequoiaInspectionAbi = SequoiaInspectionContract.abi
 export const sequoiaInvitationAbi = SequoiaInvitationContract.abi
 export const sequoiaVoteAbi = SequoiaVoteContract.abi
 export const sequoiaValidationAbi = SequoiaValidationContract.abi
+export const sequoiaRcIndexAbi = SequoiaRcIndexContract.abi
 
 /////////////////// future mainnet
 export const rcAbi = SequoiaRcTokenContract.abi
@@ -117,6 +121,7 @@ export const inspectionAbi = SequoiaInspectionContract.abi
 export const invitationAbi = SequoiaInvitationContract.abi
 export const voteAbi = SequoiaVoteContract.abi
 export const validationAbi = SequoiaValidationContract.abi
+export const rcIndexAbi = SequoiaRcIndexContract.abi
 
 export const contractsSequoia: ContractListProps[] = [
   {
@@ -201,11 +206,6 @@ export const contractsSequoia: ContractListProps[] = [
     name: SequoiaSupporterContract.contractName
   },
   {
-    abi: sequoiaSupporterPoolAbi,
-    address: sequoiaSupporterPoolAddress,
-    name: SequoiaSupporterPoolContract.contractName
-  },
-  {
     abi: sequoiaInspectionAbi,
     address: sequoiaInspectionAddress,
     name: SequoiaInspectionContract.contractName,
@@ -231,121 +231,12 @@ export const contractsSequoia: ContractListProps[] = [
     abi: sequoiaValidationAbi,
     address: sequoiaValidationAddress,
     name: SequoiaValidationContract.contractName
+  },
+  {
+    abi: sequoiaRcIndexAbi,
+    address: sequoiaRcIndexAddress,
+    name: SequoiaRcIndexContract.contractName
   }
 ]
 
-export const contractsMainnet: ContractListProps[] = [
-  {
-    abi: sequoiaRcAbi,
-    address: sequoiaRcAddress,
-    name: SequoiaRcTokenContract.contractName,
-    label: 'regenerationCredit',
-    description: 'Regeneration Credit Token Contract'
-  },
-  {
-    abi: sequoiaRcImpactAbi,
-    address: sequoiaRcImpactAddress,
-    name: SequoiaRcImpactContract.contractName
-  },
-  {
-    abi: sequoiaRegeneratorAbi,
-    address: sequoiaRegeneratorAddress,
-    name: SequoiaRegeneratorContract.contractName
-  },
-  {
-    abi: sequoiaRegeneratorPoolAbi,
-    address: sequoiaRegeneratorPoolAddress,
-    name: SequoiaRegeneratorPoolContract.contractName
-  },
-  {
-    abi: sequoiaInspectorAbi,
-    address: sequoiaInspectorAddress,
-    name: SequoiaInspectorContract.contractName
-  },
-  {
-    abi: sequoiaInspectorPoolAbi,
-    address: sequoiaInspectorPoolAddress,
-    name: SequoiaInspectorPoolContract.contractName
-  },
-  {
-    abi: sequoiaResearcherAbi,
-    address: sequoiaResearcherAddress,
-    name: SequoiaResearcherContract.contractName
-  },
-  {
-    abi: sequoiaResearcherPoolAbi,
-    address: sequoiaResearcherPoolAddress,
-    name: SequoiaResearcherPoolContract.contractName
-  },
-  {
-    abi: sequoiaDeveloperAbi,
-    address: sequoiaDeveloperAddress,
-    name: SequoiaDeveloperContract.contractName,
-    label: 'developerContract',
-    description: 'descDeveloperContract'
-  },
-  {
-    abi: sequoiaDeveloperPoolAbi,
-    address: sequoiaDeveloperPoolAddress,
-    name: SequoiaDeveloperPoolContract.contractName,
-    label: 'developerPoolContract',
-    description: 'descDeveloperPoolContract'
-  },
-  {
-    abi: sequoiaContributorAbi,
-    address: sequoiaContributorAddress,
-    name: SequoiaContributorContract.contractName
-  },
-  {
-    abi: sequoiaContributorPoolAbi,
-    address: sequoiaContributorPoolAddress,
-    name: SequoiaContributorPoolContract.contractName
-  },
-  {
-    abi: sequoiaActivistAbi,
-    address: sequoiaActivistAddress,
-    name: SequoiaActivistContract.contractName
-  },
-  {
-    abi: sequoiaActivistPoolAbi,
-    address: sequoiaActivistPoolAddress,
-    name: SequoiaActivistPoolContract.contractName
-  },
-  {
-    abi: sequoiaSupporterAbi,
-    address: sequoiaSupporterAddress,
-    name: SequoiaSupporterContract.contractName
-  },
-  {
-    abi: sequoiaSupporterPoolAbi,
-    address: sequoiaSupporterPoolAddress,
-    name: SequoiaSupporterPoolContract.contractName
-  },
-  {
-    abi: sequoiaInspectionAbi,
-    address: sequoiaInspectionAddress,
-    name: SequoiaInspectionContract.contractName,
-    label: 'inspectionContract',
-    description: 'descInspectionContract'
-  },
-  {
-    abi: sequoiaInvitationAbi,
-    address: sequoiaInvitationAddress,
-    name: SequoiaInvitationContract.contractName
-  },
-  {
-    abi: sequoiaUserAbi,
-    address: sequoiaUserAddress,
-    name: SequoiaUserContract.contractName
-  },
-  {
-    abi: voteAbi,
-    address: voteAddress,
-    name: SequoiaVoteContract.contractName
-  },
-  {
-    abi: validationAbi,
-    address: sequoiaValidationAddress,
-    name: SequoiaValidationContract.contractName
-  }
-]
+export const contractsMainnet: ContractListProps[] = []
