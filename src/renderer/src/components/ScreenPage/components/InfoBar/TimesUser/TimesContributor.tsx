50d5d64102e568516466c9380405116c1ec54cbd
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-09-02 15:04:26 -0300

    feat: create use can vote hook

--- src/renderer/src/components/ScreenPage/components/InfoBar/TimesUser/TimesContributor.tsx
+++ src/renderer/src/components/ScreenPage/components/InfoBar/TimesUser/TimesContributor.tsx
@@ -1,10 +1,12 @@
 import { useTranslation } from 'react-i18next'
 
 import { useCanPublishContribution } from '@renderer/domain/Contributor/useCases/useCanPublishContribution'
+import { useCanVote } from '@renderer/domain/Vote/useCases/useCanVote'
 
 export function TimesContributor(): JSX.Element {
   const { t } = useTranslation()
   const { canPublish, waitBlocks } = useCanPublishContribution()
+  const { isVoter, canVoteBlocks, canVoteNow } = useCanVote()
 
   return (
     <div className="flex gap-8 items-center">
@@ -13,6 +15,21 @@ export function TimesContributor(): JSX.Element {
         <span className="text-white font-semibold text-sm">
           {t(canPublish ? 'common.yes' : 'common.no')}
           {waitBlocks > 0 && ` (${t('common.wait')} ${waitBlocks} ${t('common.blocks')})`}
+        </span>
+      </p>
+
+      <p className="text-gray-300 text-xs">
+        {t('infoBar.isVoter')}:{' '}
+        <span className="text-white font-semibold text-sm">
+          {t(isVoter ? 'common.yes' : 'common.no')}
+        </span>
+      </p>
+
+      <p className="text-gray-300 text-xs">
+        {t('infoBar.canVoteNow')}:{' '}
+        <span className="text-white font-semibold text-sm">
+          {t(canVoteNow ? 'common.yes' : 'common.no')}
+          {canVoteBlocks > 0 && ` (${t('common.wait')} ${canVoteBlocks} ${t('common.blocks')})`}
         </span>
       </p>
     </div>
--- src/renderer/src/components/ScreenPage/components/InfoBar/TimesUser/TimesDeveloper.tsx
+++ src/renderer/src/components/ScreenPage/components/InfoBar/TimesUser/TimesDeveloper.tsx
@@ -1,10 +1,12 @@
 import { useTranslation } from 'react-i18next'
 
 import { useCanPublishReport } from '@renderer/domain/Developer/useCases/useCanPublishReport'
+import { useCanVote } from '@renderer/domain/Vote/useCases/useCanVote'
 
 export function TimesDeveloper(): JSX.Element {
   const { t } = useTranslation()
   const { canPublish, waitBlocks } = useCanPublishReport()
+  const { isVoter, canVoteBlocks, canVoteNow } = useCanVote()
 
   return (
     <div className="flex gap-8 items-center">
@@ -13,6 +15,21 @@ export function TimesDeveloper(): JSX.Element {
         <span className="text-white font-semibold text-sm">
           {t(canPublish ? 'common.yes' : 'common.no')}
           {waitBlocks > 0 && ` (${t('common.wait')} ${waitBlocks} ${t('common.blocks')})`}
+        </span>
+      </p>
+
+      <p className="text-gray-300 text-xs">
+        {t('infoBar.isVoter')}:{' '}
+        <span className="text-white font-semibold text-sm">
+          {t(isVoter ? 'common.yes' : 'common.no')}
+        </span>
+      </p>
+
+      <p className="text-gray-300 text-xs">
+        {t('infoBar.canVoteNow')}:{' '}
+        <span className="text-white font-semibold text-sm">
+          {t(canVoteNow ? 'common.yes' : 'common.no')}
+          {canVoteBlocks > 0 && ` (${t('common.wait')} ${canVoteBlocks} ${t('common.blocks')})`}
         </span>
       </p>
     </div>
