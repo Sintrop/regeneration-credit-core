4dc596880a3efac3284e67723516defe1d000d62
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-10-01 10:06:50 -0300

    feat: remove times can vote

--- src/renderer/src/components/ScreenPage/components/InfoBar/TimesUser/TimesActivist.tsx
+++ src/renderer/src/components/ScreenPage/components/InfoBar/TimesUser/TimesActivist.tsx
@@ -1,26 +1,3 @@
-import { useCanVote } from '@renderer/domain/Vote/useCases/useCanVote'
-import { useTranslation } from 'react-i18next'
-
 export function TimesActivist(): JSX.Element {
-  const { t } = useTranslation()
-  const { isVoter, canVoteBlocks, canVoteNow } = useCanVote()
-
-  return (
-    <div className="flex gap-8 items-center">
-      <p className="text-gray-300 text-xs">
-        {t('infoBar.isVoter')}:{' '}
-        <span className="text-white font-semibold text-sm">
-          {t(isVoter ? 'common.yes' : 'common.no')}
-        </span>
-      </p>
-
-      <p className="text-gray-300 text-xs">
-        {t('infoBar.canVoteNow')}:{' '}
-        <span className="text-white font-semibold text-sm">
-          {t(canVoteNow ? 'common.yes' : 'common.no')}
-          {canVoteBlocks > 0 && ` (${t('common.wait')} ${canVoteBlocks} ${t('common.blocks')})`}
-        </span>
-      </p>
-    </div>
-  )
+  return <div className="flex gap-8 items-center"></div>
 }
